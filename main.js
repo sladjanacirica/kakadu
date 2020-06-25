13957b7a4ec36799747c922fe73ea6a976eda0c7
sladjanacirica <39948955+sladjanacirica@users.noreply.github.com>
2020-06-25 18:53:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+(function() {
+  var app;
+
+  $(document).ready(function() {
+    return app.init();
+  });
+
+  app = {
+    text: "DOBRODOŠLI",
+    index: 0,
+    chars: 0,
+    speed: 150,
+    container: ".content",
+    init: function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+    write: function() {
+      $(this.container).append(this.text[this.index]);
+      if (this.index < this.chars) {
+        this.index++;
+        return window.setTimeout(function() {
+          return app.write();
+        }, this.speed);
+      }
+    }
+  };
+
+}).call(this);
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
